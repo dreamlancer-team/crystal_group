4bba62ab1e29a0da6cfd398e2560395bdeed76be
unknown <aniketkhote99@gmail.com>
2022-02-04 23:53:48 +0530

    refactor code

--- resources/views/user/layout/header.blade.php
+++ resources/views/user/layout/header.blade.php
@@ -3,17 +3,13 @@
         <div class="row">
             <div class="col-12">
                 <nav class="main-nav">
-                    <!-- ***** Logo Start ***** -->
                     <a href="/" class="logo">
-                        <img src="user/assets/images/logo.png" alt="logo"/>
+                        <img src="user/assets/images/logo.jpeg" style="width: 150px; height:75px; margin-top:-23px" alt="logo"/>
                     </a>
-                    <!-- ***** Logo End ***** -->
-                    <!-- ***** Menu Start ***** -->
                     <ul class="nav">
                         <li><a href="/">Home</a></li>
                         <li><a href="/#product">Products</a></li>
                         <li><a href="/#services">Services</a></li>
-                        {{-- <li><a href="/#testimonials">Testimonials</a></li> --}}
                         <li><a href="/#client">Clients</a></li>
                         <li><a href="/#about">About</a></li>
                         <li><a href="/#contact">Contact Us</a></li>
@@ -21,7 +17,6 @@
                     <a class='menu-trigger'>
                         <span>Menu</span>
                     </a>
-                    <!-- ***** Menu End ***** -->
                 </nav>
             </div>
         </div>
---
+++ resources/views/user/partials/big_features.blade.php
@@ -0,0 +1,37 @@
+<section class="section padding-top-70 padding-bottom-0" id="features">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 col-md-12 col-sm-12 align-self-center" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                <img src="user/assets/images/left-image.png" class="rounded w-50 img-fluid d-block mx-auto" alt="App">
+            </div>
+            <div class="col-lg-1"></div>
+            <div class="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-top-fix">
+                <div class="left-heading">
+                    <h2 class="section-title">Let’s discuss about you project</h2>
+                </div>
+                <div class="left-text">
+                    <p>{{ $item->get('feature_1') }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section padding-bottom-100">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-bottom-fix">
+                <div class="left-heading">
+                    <h2 class="section-title">We can help you to grow your business</h2>
+                </div>
+                <div class="left-text">
+                    <p>{{ $item->get('feature_2') }}</p>
+                </div>
+            </div>
+            <div class="col-lg-1"></div>
+            <div class="col-lg-5 col-md-12 col-sm-12 align-self-center mobile-bottom-fix-big" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+                <img src="user/assets/images/right-image.png" class="rounded w-50 img-fluid d-block mx-auto" alt="App">
+            </div>
+        </div>
+    </div>
+</section>
